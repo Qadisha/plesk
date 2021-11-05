993ce3bc6b6fc7452df7b432f3a9b0b337c8870c
Qadisha <info@qadisha.it>
2021-11-05 03:57:11 +0100

    backup single domain using Plesk CLI

---
+++ backup-domain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+DOMAIN="domainname"
+FTP_USER="ftpusername"
+FTP_PASS="ftppassword"
+FTP_HOST="ftpserver"
+FTP_PATH="ftppath"
+
+plesk bin pleskbackup --domains-name ${DOMAIN} -exclude-logs --output-file=ftp://${FTP_USER}:${FTP_PASS}@${FTP_HOST}/${FTP_PATH}/`date +%F`_${DOMAIN}.raw
+
+
+
