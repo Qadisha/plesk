edf018e0d6e53fd4d7ae8819fbcd102048faae79
Qadisha <info@qadisha.it>
2021-11-05 05:50:42 +0100

    General update on file names

--- backup-db.sh
+++ backup-db.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+set -e
+
 DBNAME="database"
 FILENAME="/home/database.sql"
 
 mysqldump -uadmin -p`cat /etc/psa/.psa.shadow` $DBNAME > $FILENAME
-
 
 
 
--- backup-domain.sh
+++ backup-domain.sh
@@ -7,7 +7,7 @@ FTP_PASS="ftppassword"
 FTP_HOST="ftpserver"
 FTP_PATH="ftppath"
 
-plesk bin pleskbackup --domains-name ${DOMAIN} -exclude-logs --output-file=ftp://${FTP_USER}:${FTP_PASS}@${FTP_HOST}/${FTP_PATH}/`date +%F`_${DOMAIN}.raw
+plesk bin pleskbackup --domains-name ${DOMAIN} -exclude-logs --output-file=ftp://${FTP_USER}:${FTP_PASS}@${FTP_HOST}/${FTP_PATH}/`date +%Y-%m-%d_%H-%M`_${DOMAIN}.raw
 
 
 
